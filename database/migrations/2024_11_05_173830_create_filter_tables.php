4f1d1966a88c45c6d3b31ad03f941846bb6666bf
Razvan Ciomaga <razvan.ciomaga@5psolutions.eu>
2024-11-05 19:51:16 +0200

    filter tables migration

---
+++ database/migrations/2024_11_05_173830_create_filter_tables.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('counties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('towns', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('zones', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('room_entrances', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('years', function (Blueprint $table) {
+            $table->id();
+            $table->year('name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filter_tables');
+        Schema::dropIfExists('counties');
+        Schema::dropIfExists('towns');
+        Schema::dropIfExists('zones');
+        Schema::dropIfExists('room_entrances');
+        Schema::dropIfExists('years');
+    }
+};
