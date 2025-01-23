b16bf200d7878cc9c11f6abd3a5629a5d0578054
Razvan Ciomaga <razvan.ciomaga@5psolutions.eu>
2025-01-23 15:12:02 +0200

    remove title from home page

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,15 +1,6 @@
 <div>
     <!-- ##### Breadcumb Area Start ##### -->
     <section class="breadcumb-area bg-img" style="background-image: url(img/bg-img/hero1.jpg);">
-        <div class="container h-100">
-            <div class="row h-100 align-items-center">
-                <div class="col-12">
-                    <div class="breadcumb-content">
-                        <h3 class="breadcumb-title">{{ label('Pagina principala') }}</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
     </section>
     <!-- ##### Breadcumb Area End ##### -->
 
