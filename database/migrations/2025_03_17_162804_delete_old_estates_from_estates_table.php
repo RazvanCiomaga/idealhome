e2a04afd42d5e544e37816822674c76e124b5d37
Razvan Ciomaga <razvan.ciomaga@5psolutions.eu>
2025-03-17 17:14:45 +0200

    delete old imobmanager estates

---
+++ database/migrations/2025_03_17_162804_delete_old_estates_from_estates_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Estate;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Estate::query()->whereNull('crm_id')->delete();
+    }
+};
